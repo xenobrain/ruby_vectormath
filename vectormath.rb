14bc6ccb6bac40f79056ae86c5bd70014b15dbf1
xenobrain <xenobrain@gmail.com>
2021-07-09 10:12:17 -0700

    rewrite vec2

--- vectormath.rb
+++ vectormath.rb
@@ -1,121 +1,150 @@
 PI_OVER_180 = Math::PI / 180.0
 
 # Vector2 ##############################################################################################################
-Vec2 = Struct.new(:x, :y)
+class Vec2
+  attr_accessor(:x, :y)
 
-def vec2_dot(vec2_a, vec2_b)
-  vec2_a.x * vec2_b.x + vec2_a.y * vec2_b.y
-end
+  def initialize(x = 0.0, y = 0.0)
+    @x = x
+    @y = y
+  end
 
-def vec2_cross(vec2_lhs, vec2_rhs)
-  vec2_lhs.x * vec2_rhs.y - vec2_lhs.y * vec2_rhs.y
-end
+  def add!(vec2_rhs)
+    @x += vec2_rhs.x
+    @y += vec2_rhs.y
+  end
 
-def vec2_add(vec2_out, vec2_lhs, vec2_rhs)
-  vec2_out.x = vec2_lhs.x + vec2_rhs.x
-  vec2_out.y = vec2_lhs.y + vec2_rhs.y
-  vec2_out
-end
+  def add2_self!(vec2_lhs, vec2_rhs)
+    @x = vec2_lhs.x + vec2_rhs.x
+    @y = vec2_rhs.y + vec2_rhs.y
+  end
 
-def vec2_sub(vec2_out, vec2_lhs, vec2_rhs)
-  vec2_out.x = vec2_lhs.x - vec2_rhs.x
-  vec2_out.y = vec2_lhs.y - vec2_rhs.y
-  vec2_out
-end
+  def add2!(vec2_lhs, vec2_rhs)
+    @x = vec2_lhs.x + vec2_rhs.x
+    @y = vec2_rhs.y + vec2_rhs.y
+  end
 
-def vec2_mul(vec2_out, vec2_lhs, scalar_rhs)
-  vec2_out.x = vec2_lhs.x * scalar_rhs
-  vec2_out.y = vec2_lhs.y * scalar_rhs
-  vec2_out
-end
+  def sub!(vec2_rhs)
+    @x -= vec2_rhs.x
+    @y -= vec2_rhs.y
+  end
 
-def vec2_mul_add(vec2_out, vec2_lhs, scalar_rhs)
-  vec2_out = vec2_lhs.x * scalar_rhs
-  vec2_out = vec2_lhs.y * scalar_rhs
-  vec2_out
-end
+  def sub2!(vec2_lhs, vec2_rhs)
+    @x = vec2_lhs.x - vec2_rhs.x
+    @y = vec2_lhs.y - vec2_lhs.y
+  end
 
-def vec2_mul_mat2(vec2_out, vec2_a, mat2_a)
-  vec2_out.x = mat2_a.x.x * vec2_a.x + mat2_a.y.x * vec2_a.y
-  vec2_out.y = mat2_a.x.y * vec2_a.x + mat2_a.y.y * vec2_a.y
-  vec2_out
-end
+  def mul!(scalar_rhs)
+    @x *= scalar_rhs
+    @y *= scalar_rhs
+  end
 
-def vec2_element_mul(vec2_out, vec2_lhs, vec2_rhs)
-  vec2_out.x = vec2_lhs.x * vec2_rhs.x
-  vec2_out.y = vec2_lhs.y * vec2_rhs.y
-  vec2_out
-end
+  def mul2!(vec2_lhs, scalar_rhs)
+    @x = vec2_lhs.x * scalar_rhs
+    @y = vec2_lhs.y * scalar_rhs
+  end
 
-def vec2_element_div(vec2_out, vec2_lhs, vec2_rhs)
-  vec2_out.x = vec2_lhs.x / vec2_rhs.x
-  vec2_out.y = vec2_lhs.y / vec2_rhs.y
-  vec2_out
-end
+  def mul_add!(vec2_rhs, scalar_mul)
+    @x += vec2_rhs * scalar_mul
+    @y += vec2_rhs * scalar_mul
+  end
 
-def vec2_min(vec2_out, vec2_a, vec2_b)
-  vec2_out.x = vec2_a.x < vec2_b.x ? vec2_a.x : vec2_b.x
-  vec2_out.y = vec2_a.y < vec2_b.y ? vec2_a.y : vec2_b.y
-  vec2_out
-end
+  def mul_add2!(vec2_lhs, vec2_rhs, scalar_rhs)
+    @x = vec2_lhs.x + vec2_rhs.x * scalar_rhs
+    @y = vec2_lhs.y + vec2_rhs.y * scalar_rhs
+  end
 
-def vec2_max(vec2_out, vec2_a, vec2_b)
-  vec2_out.x = vec2_a.x > vec2_b.x ? vec2_a.x : vec2_b.x
-  vec2_out.y = vec2_a.y > vec2_b.y ? vec2_a.y : vec2_b.y
-  vec2_out
-end
+  def element_mul!(vec2_rhs)
+    @x *= vec2_rhs.x
+    @y *= vec2_rhs.x
+  end
 
-def vec2_abs(vec2_out, vec2_a)
-  vec2_out.x = vec2_a.x.abs
-  vec2_out.y = vec2_a.y.abs
-  vec2_out
-end
+  def element_mul2!(vec2_lhs, vec2_rhs)
+    @x = vec2_lhs.x * vec2_rhs.x
+    @y = vec2_lhs.y * vec2_rhs.y
+  end
 
-def vec2_eq(vec2_a, vec2_b)
-  vec2_a.x == vec2_b.x && vec2_a.y == vec2_b.y
-end
+  def element_div!(vec2_rhs)
+    @x /= vec2_rhs.x
+    @y /= vec2_rhs.y
+  end
 
-def vec2_lerp(vec2_out, vec2_a, vec2_b, scalar_t)
-  vec2_out.x = vec2_a.x + scalar_t * (vec2_b.x - vec2_a.x)
-  vec2_out.y = vec2_a.y + scalar_t * (vec2_b.y - vec2_a.y)
-  vec2_out
-end
+  def element_div2!(vec2_lhs, vec2_rhs)
+    @x = vec2_lhs.x / vec2_rhs.x
+    @y = vec2_lhs.y / vec2_rhs.y
+  end
 
-def vec2_scale(vec2_out, vec2_a, scalar)
-  vec2_out.x = vec2_a.x * scalar
-  vec2_out.y = vec2_a.y * scalar
-  vec2_out
-end
+  def dot(vec2_rhs)
+    @x * vec2_rhs.x + @y * vec2_rhs.y
+  end
 
-def vec2_rotate_by(vec2_out, vec2_a, scalar_degrees, vec2_center)
-  radians = scalar_degrees * PI_OVER_180
-  cos = Math.cos(radians)
-  sin = Math.sin(radians)
+  def cross(vec2_rhs)
+    @y * vec2_rhs.x - @y * vec2_rhs.y
+  end
 
-  vec2_out.x = vec2_a.x - vec2_center.x
-  vec2_out.y = vec2_a.y - vec2_center.y
-  vec2_out.x = (vec2_out.x * cos - vec2_out.y * sin) + vec2_center.x
-  vec2_out.y = (vec2_out.x * sin + vec2_out.y * cos) + vec2_center.y
-  vec2_out
-end
+  def min!(vec2_rhs)
+    @x = @x < vec2_rhs.x ? @x : vec2_rhs.x
+    @y = @y < vec2_rhs.y ? @y : vec2_rhs.y
+  end
 
-def vec2_distance(vec2_a, vec2_b)
-  Math.sqrt(vec2_a.x * vec2_a.x + vec2_a.y * vec2_a.y) - Math.sqrt(vec2_b.x * vec2_b.x + vec2_b.y * vec2_b.y)
-end
+  def min2!(vec2_lhs, vec2_rhs)
+    @x = vec2_lhs.x < vec2_rhs.x ? vec2_lhs.x : vec2_rhs.x
+    @y = vec2_lhs.y < vec2_rhs.y ? vec2_lhs.y : vec2_rhs.y
+  end
 
-def vec2_length(vec2_a)
-  Math.sqrt(vec2_a.x * vec2_a.x + vec2_a.y * vec2_a.y)
-end
+  def max!(vec2_rhs)
+    @x = @x > vec2_rhs.x ? @x : vec2_rhs.x
+    @y = @y > vec2_rhs.y ? @y : vec2_rhs.y
+  end
 
-def vec2_length_sq(vec2_a)
-  vec2_a.x * vec2_a.x + vec2_a.y * vec2_a.y
-end
+  def max2!(vec2_lhs, vec2_rhs)
+    @x = vec2_lhs.x > vec2_rhs.x ? vec2_lhs.x : vec2_rhs.x
+    @y = vec2_lhs.y > vec2_rhs.y ? vec2_lhs.y : vec2_rhs.y
+  end
 
-def vec2_normalize(vec2_out, vec2_a)
-  inverse_length = 1.0 / Math.sqrt(vector_a.x * vector_a.x + vector_a.y * vector_a.y)
-  vec2_out.x = vec2_a.x * inverse_length
-  vec2_out.y = vec2_a.y * inverse_length
-  vec2_out
+  def abs!
+    @x = @x.abs
+    @y = @y.abs
+  end
+
+  def lerp!(vec2_rhs, scalar_t)
+    @x += scalar_t * (vec2_rhs.x - @x)
+    @y += scalar_t * (vec2_rhs.y - @y)
+  end
+
+  def lerp2!(vec2_lhs, vec2_rhs, scalar_t)
+    @x = vec2_lhs.x + scalar_t * (vec2_rhs.x - vec2_lhs.x)
+    @y = vec2_lhs.y + scalar_t * (vec2_rhs.y - vec2_lhs.y)
+  end
+
+  def distance(vec2_rhs)
+    Math.sqrt(@x * @x + @y * @y) - Math.sqrt(vec2_rhs.x * vec2_rhs.x + vec2_rhs.y * vec2_rhs.y)
+  end
+
+  def length
+    Math.sqrt(@x * @x + @y * @y)
+  end
+
+  def length_sq
+    @x * @x + @y * @y
+  end
+
+  def normalize!
+    inverse_length = 1.0 / Math.sqrt(@x * @x + @y * @y)
+    @x *= inverse_length
+    @y *= inverse_length
+  end
+
+  def rotate_by!(vec2_center, scalar_degrees)
+    radians = scalar_degrees * PI_OVER_180
+    cos = Math.cos(radians)
+    sin = Math.sin(radians)
+
+    @x -= vec2_center.x
+    @y -= vec2_center.y
+    @x = (@x * cos - @y * sin) + vec2_center.x
+    @y = (@x * sin + @y * cos) + vec2_center.y
+  end
 end
 
 # Matrix 2x2 ###########################################################################################################
